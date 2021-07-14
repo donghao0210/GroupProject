0bc068858d16092b59a3ca043c8593ebd916f478
alvinyeoh___ <alvin.yeoh@mycareconcierge.com>
2021-07-14 14:58:58 +0800

    further improved controllers

--- controllers/db.php
+++ controllers/db.php
@@ -14,55 +14,55 @@
     return $conn;
   }
 
-  function userRegister($email, $name, $password, $datetime) {
-    $conn = connectDatabase();     // connect to database
-    $sql = "INSERT INTO `user` (email, `name`, `password`, created_at) VALUES (?, ?, ?, ?);";
+  // function userRegister($email, $name, $password, $datetime) {
+  //   $conn = connectDatabase();     // connect to database
+  //   $sql = "INSERT INTO `user` (email, `name`, `password`, created_at) VALUES (?, ?, ?, ?);";
 
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ssss", $email, $name, $password, $datetime);
+  //   $stmt = $conn->prepare($sql);
+  //   $stmt->bind_param("ssss", $email, $name, $password, $datetime);
 
-    if($stmt->execute()) {
-      echo "Registration Successful!";
-    }
-    else {
-      echo  $conn->error;
-    }
+  //   if($stmt->execute()) {
+  //     echo "Registration Successful!";
+  //   }
+  //   else {
+  //     echo  $conn->error;
+  //   }
 
-    $stmt->close();
-    $conn->close();
+  //   $stmt->close();
+  //   $conn->close();
 
-  }
+  // }
 
-  function userLogin($email, $password) {
-    $conn = connectDatabase();     // connect to database
-    $sql = "SELECT name, email, password FROM user WHERE email = ?";    // sql command to select user information from the database with user input email
+  // function userLogin($email, $password) {
+  //   $conn = connectDatabase();     // connect to database
+  //   $sql = "SELECT name, email, password FROM user WHERE email = ?";    // sql command to select user information from the database with user input email
 
-    $stmt = $conn->prepare($sql);   // prepare sql query statement
-    $stmt->bind_param("s", $email); // bind the user input email into the sql command and assign into $stmt
+  //   $stmt = $conn->prepare($sql);   // prepare sql query statement
+  //   $stmt->bind_param("s", $email); // bind the user input email into the sql command and assign into $stmt
 
-    if($stmt->execute()) {      // if successfully execute $stmt
-      $stmt->bind_result($name, $email, $pass);   // bind the result into the variables
-      if($stmt->fetch()) {
-          if(strcmp($password, $pass) == 0) {     // if user input password is same as the password retrived from the database
-              $_SESSION["loggedin"] = true;       // assign the variables into $_SESSION
-              $_SESSION["name"] = $name;
-              $_SESSION["email"] = $email;
-          }
-          else {
-            $errorMsg = "Login Failed. Invalid Email/Password.";
-          }
-      }
-      else {
-        $errorMsg = "Login Failed. Invalid Email/Password.";
-      }
-    }
-    else {
-      $errorMsg = $conn->error;
-    }
+  //   if($stmt->execute()) {      // if successfully execute $stmt
+  //     $stmt->bind_result($name, $email, $pass);   // bind the result into the variables
+  //     if($stmt->fetch()) {
+  //         if(strcmp($password, $pass) == 0) {     // if user input password is same as the password retrived from the database
+  //             $_SESSION["loggedin"] = true;       // assign the variables into $_SESSION
+  //             $_SESSION["name"] = $name;
+  //             $_SESSION["email"] = $email;
+  //         }
+  //         else {
+  //           $errorMsg = "Login Failed. Invalid Email/Password.";
+  //         }
+  //     }
+  //     else {
+  //       $errorMsg = "Login Failed. Invalid Email/Password.";
+  //     }
+  //   }
+  //   else {
+  //     $errorMsg = $conn->error;
+  //   }
 
-    $stmt->close();     // close statement
-    $conn->close();     // close database connection
+  //   $stmt->close();     // close statement
+  //   $conn->close();     // close database connection
 
-    return $errorMsg;
-  }
+  //   return $errorMsg;
+  // }
 ?>
---
+++ controllers/userController.php
@@ -0,0 +1,65 @@
+<?php
+    require_once 'db.php';
+    
+    function userRegister($email, $name, $password, $datetime) {
+        $conn = connectDatabase();     // connect to database
+        $sql = "INSERT INTO `user` (email, `name`, `password`, created_at) VALUES (?, ?, ?, ?);";
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("ssss", $email, $name, $password, $datetime);
+
+        if($stmt->execute()) {
+        echo "Registration Successful!";
+        }
+        else {
+        echo  $conn->error;
+        }
+
+        $stmt->close();
+        $conn->close();
+
+    }
+
+    function userLogin($email, $password) {
+        $conn = connectDatabase();     // connect to database
+        $sql = "SELECT name, email, password FROM user WHERE email = ?";    // sql command to select user information from the database with user input email
+    
+        $stmt = $conn->prepare($sql);   // prepare sql query statement
+        $stmt->bind_param("s", $email); // bind the user input email into the sql command and assign into $stmt
+    
+        if($stmt->execute()) {      // if successfully execute $stmt
+          $stmt->bind_result($name, $email, $pass);   // bind the result into the variables
+          if($stmt->fetch()) {
+              if(strcmp($password, $pass) == 0) {     // if user input password is same as the password retrived from the database
+                  $_SESSION["loggedin"] = true;       // assign the variables into $_SESSION
+                  $_SESSION["name"] = $name;
+                  $_SESSION["email"] = $email;
+              }
+              else {
+                $msg = "Login Failed. Invalid Email/Password.";
+              }
+          }
+          else {
+            $msg = "Login Failed. Invalid Email/Password.";
+          }
+        }
+        else {
+          $msg = $conn->error;
+        }
+    
+        $stmt->close();     // close statement
+        $conn->close();     // close database connection
+
+        if(!isset($msg)) {
+            $msg = "Login Successfully";
+        }
+    
+        return $msg;
+      }
+
+    function userLogout() {
+        session_destroy();
+        header("Location: login.php");
+    }
+
+?>
