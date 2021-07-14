d6e5a4b260bb493f217e354454a594e71200098a
donghao0210 <donghao0210@users.noreply.github.com>
2021-07-14 13:52:50 +0800

    added Empty string checking before posting

--- view/index.php
+++ view/index.php
@@ -92,10 +92,12 @@
 
       <?php
         if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
-          if(isset($_POST['post_content']) && isset($_POST['content'])){
+          if(isset($_POST['post_content']) && (isset($_POST['content']) && $_POST['content'] !="")){
               $content = $_POST['content'];
               $user_id = $_SESSION["user_id"];
               addPost($user_id, $content);
+          }else{
+            echo '<script>alert("Cannot Post Emtpy thoughs :(")</script>';
           }
         }
         else if(!isset($_SESSION['loggedin'])){
