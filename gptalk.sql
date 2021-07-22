caab40bc40365b15547620e7ec78b7c1f2e7ba70
alvinyeoh___ <alvin.yeoh@mycareconcierge.com>
2021-07-22 10:22:36 +0800

    delete all the data in database

--- gptalk.sql
+++ gptalk.sql
@@ -32,19 +32,13 @@ USE `gptalk`;
 DROP TABLE IF EXISTS `comment`;
 CREATE TABLE `comment` (
   `comment_id` int(10) NOT NULL,
-  `post_id` int(11) DEFAULT NULL,
+  `post_id` int(11) NOT NULL,
   `created_by` int(11) UNSIGNED NOT NULL,
   `content` varchar(150) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `comment`
---
-
-INSERT INTO `comment` (`comment_id`, `post_id`, `created_by`, `content`, `created_at`, `updated_at`) VALUES
-(10, 23, 1, 'i know', '2021-07-14 12:04:37', '2021-07-14 12:04:37');
 
 -- --------------------------------------------------------
 
@@ -61,19 +55,6 @@ CREATE TABLE `post` (
   `updated_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `post`
---
-
-INSERT INTO `post` (`post_id`, `created_by`, `content`, `created_at`, `updated_at`) VALUES
-(5, 1, 'hello', '2021-07-14 08:12:29', '2021-07-14 08:12:29'),
-(6, 6, 'asd', '2021-07-14 08:18:42', '2021-07-14 08:18:42'),
-(7, 6, 'hello world', '2021-07-14 08:30:05', '2021-07-14 08:30:05'),
-(18, 1, '123', '2021-07-14 09:10:35', '2021-07-14 09:10:35'),
-(21, 6, 'hello', '2021-07-14 09:13:13', '2021-07-14 09:13:13'),
-(22, 1, 'adsadsad', '2021-07-14 11:28:41', '2021-07-14 11:28:41'),
-(23, 1, 'i am here', '2021-07-14 12:04:31', '2021-07-14 12:04:31'),
-(24, 1, 'asdzzzzzzz', '2021-07-14 13:31:33', '2021-07-14 13:31:33');
 
 -- --------------------------------------------------------
 
@@ -90,28 +71,7 @@ CREATE TABLE `user` (
   `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `user`
---
 
-INSERT INTO `user` (`id`, `email`, `name`, `password`, `created_at`) VALUES
-(1, 'test@gmail.com', 'Test User', '12345', '2021-06-29 21:15:22'),
-(6, 'a@a.com', 'Lee Dong Hao', 'asc', '2021-07-12 10:25:20'),
-(8, 'donghao20082001@gmail.com', 'a', 'aa', '2021-07-12 10:25:58'),
-(9, 'user123@example.com', 'c', 'zxc', '2021-07-12 10:26:25'),
-(12, 'leedonghao@sd.taylors.edu.my', 'e', 'zxc', '2021-07-12 10:26:46'),
-(14, 'aaa@mail.com', 'g', '2168546846', '2021-07-12 14:07:12'),
-(15, 'a@aaa.com', 'cz', '5555555', '2021-07-14 03:06:36'),
-(24, 'test@gmail.com12', 'asd', 'asdad', '2021-07-14 03:10:58'),
-(26, 'test@gmail.comaaa', 'c', 'ads', '2021-07-14 03:18:53'),
-(28, 'test@gmail.comasda', 'Lee Dong Hzcxzcxao', '123', '2021-07-14 03:27:13'),
-(30, 'test@gmail.comasdaa', '12', '123', '2021-07-14 03:27:30'),
-(32, 'test@gmail.comasdaaa', 'Lee Dong 3333', '123', '2021-07-14 03:27:39'),
-(33, 'test@gmail.comasdaaa1', '15323', '123', '2021-07-14 03:28:14'),
-(35, 'test@gmail.comasdaaa1a', 'alvin', '123ads', '2021-07-14 03:28:20'),
-(36, 'test@gmail.comasdaaa1aa', 'b', '123ads', '2021-07-14 03:29:53'),
-(37, 'donghao20082001@gmail.comads', '1231241', 'dad', '2021-07-14 03:30:11'),
-(38, 'donghao20082001@gmail.comadsa', 'zzzzzzzzz', 'asd', '2021-07-14 03:30:49');
 
 --
 -- Indexes for dumped tables
